1f5d10f661844ebf4e839065443929970db4f371
juanguerrero09 <juanguerrero09mc@gmail.com>
2023-10-05 23:55:33 +0200

    colly scraper added

--- cmd/scraper/main.go
+++ cmd/scraper/main.go
@@ -1,11 +1,30 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"log"
+	"os"
 	"sound/pkg"
+
+	"github.com/gocolly/colly"
 )
 
 func main() {
 	pkg.Scraper()
 	fmt.Println("Hola mundo")
+	fName := "data.csv"
+	file, err := os.Create(fName)
+	if err != nil {
+		log.Fatalf("could not create te file, err: %q", err)
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	c := colly.NewCollector(
+		colly.AllowedDomains("aliexpress.com"),
+	)
+
 }
