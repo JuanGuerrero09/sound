44727689e68680e3ce164eb8232f515d74cd2616
juanguerrero09 <juanguerrero09mc@gmail.com>
2023-10-07 00:18:05 +0200

    add example for colly

--- cmd/scraper/main.go
+++ cmd/scraper/main.go
@@ -1,30 +1,58 @@
 package main
 
 import (
-	"encoding/csv"
 	"fmt"
-	"log"
-	"os"
-	"sound/pkg"
+	// "encoding/csv"
+	// "log"
+	// "os"
+	// "sound/pkg"
 
 	"github.com/gocolly/colly"
 )
 
 func main() {
-	pkg.Scraper()
-	fmt.Println("Hola mundo")
-	fName := "data.csv"
-	file, err := os.Create(fName)
-	if err != nil {
-		log.Fatalf("could not create te file, err: %q", err)
-	}
-	defer file.Close()
+	// pkg.Scraper()
+	// fmt.Println("Hola mundo")
+	// fName := "data.csv"
+	// file, err := os.Create(fName)
+	// if err != nil {
+	// 	log.Fatalf("could not create te file, err: %q", err)
+	// }
+	// defer file.Close()
 
-	writer := csv.NewWriter(file)
-	defer writer.Flush()
+	// writer := csv.NewWriter(file)
+	// defer writer.Flush()
 
-	c := colly.NewCollector(
-		colly.AllowedDomains("aliexpress.com"),
-	)
+	// c := colly.NewCollector(
+	// 	colly.AllowedDomains("http://quotes.toscrape.com"),
+	// )
+
+	// c.OnRequest(func(r *colly.Request) {
+	// 	 r.Headers.Set("User-Agent")
+	// 	fmt.Println("Visiting: ", r.URL)
+	// })
+
+	// c.OnResponse(func(r *colly.Response) {
+	// 	fmt.Println("Response code: ", r.StatusCode)
+	// })
+
+	// c.OnError(func(r *colly.Response, err error) {
+	// 	fmt.Println("error", err.Error())
+	// })
+
+	
+	c := colly.NewCollector()
+	
+	// Find and visit all links
+	c.OnHTML(".text", func(e *colly.HTMLElement) {
+		fmt.Println(e)
+	})
+	
+	c.OnHTML(".author", func(h *colly.HTMLElement) {
+		fmt.Println("Author: ", h.Text)
+	})
+	c.Visit("https://es.aliexpress.com/item/1005005816349636.html")
+
+
 
 }
